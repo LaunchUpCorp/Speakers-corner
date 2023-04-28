1bfc87390e811c184d3f3a0c8bbd03d1d3dee37c
Moin Sekander <109125797+m-sekander@users.noreply.github.com>
2023-04-28 00:14:10 -0400

    [SPE-2] Chore: create boiler plate, install discussed packages, init browser routes, make app hello world (#1)
    Co-authored-by: YoussefLaunchUp <104530466+YoussefLaunchUp@users.noreply.github.com>

---
+++ client/src/App.tsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="flex min-h-screen items-center justify-center">
+              <h1 className="mb-5 rounded-xl border bg-gray-200 px-16 py-8 text-3xl font-bold text-gray-700 shadow">
+                Hello World !
+              </h1>
+            </div>
+          }
+        />
+        <Route />
+        <Route />
+        <Route />
+        <Route />
+        <Route />
+        <Route />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
