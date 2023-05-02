3b9bf75bb7cfee928510f315da23f15af6a09ca3
Moin Sekander <moinuddin8510@gmail.com>
2023-05-02 17:47:35 -0400

    feat: create header component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,31 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div className="flex min-h-screen items-center justify-center">
-              <h1 className="mb-5 rounded-xl border bg-gray-200 px-16 py-8 text-3xl font-bold text-gray-700 shadow">
-                Hello World !
-              </h1>
-            </div>
-          }
-        />
-        <Route />
-        <Route />
-        <Route />
-        <Route />
-        <Route />
-        <Route />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+      <main className="min-h-screen">
+        <Header />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div className="flex min-h-[calc(100vh-4.5rem)] items-center justify-center">
+                <h1 className="rounded-xl border bg-gray-200 px-8 py-4 text-lg font-bold text-gray-700 shadow">
+                  Hello World !
+                </h1>
+              </div>
+            }
+          />
+          <Route />
+          <Route />
+          <Route />
+          <Route />
+          <Route />
+          <Route />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </main>
     </BrowserRouter>
   );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "speakers-corner-blue": "#0077c0",
+      },
+    },
   },
   plugins: [],
 };
