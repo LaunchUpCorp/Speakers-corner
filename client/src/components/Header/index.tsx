1007ed62a11a76b5886a691820315c33ce2214b5
Moin Sekander <moinuddin8510@gmail.com>
2023-05-02 20:22:04 -0400

    refactor: adjust header to have a breakpoint at md and a max width of 7xl as discussed by team

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -4,17 +4,23 @@ import NavItem from "./NavItem";
 
 function Header() {
   return (
-    <header className="flex flex-col items-center gap-2 bg-sc-blue p-2 sm:flex-row sm:items-center sm:justify-between">
-      <Link to="/">
-        <img className="h-14 w-fit" src={blueLogo} alt="speakers corner logo" />
-      </Link>
-      <nav className="flex flex-wrap justify-center gap-x-3 gap-y-2 text-sm text-white sm:gap-3">
-        <NavItem label="Home" path="/" endPath={true} />
-        <NavItem label="Create Room" path="/create-room" endPath={true} />
-        <NavItem label="Profile" path="/profile" endPath={true} />
-        <NavItem label="Login" path="/login" endPath={true} />
-        <NavItem label="Sign Up" path="/signup" endPath={true} />
-      </nav>
+    <header className="flex justify-center bg-sc-blue p-2">
+      <div className="flex w-full max-w-7xl flex-col items-center gap-2 md:flex-row md:items-center md:justify-between">
+        <Link to="/">
+          <img
+            className="h-14 w-fit"
+            src={blueLogo}
+            alt="speakers corner logo"
+          />
+        </Link>
+        <nav className="flex flex-wrap justify-center gap-x-3 gap-y-2 text-sm text-white md:gap-3">
+          <NavItem label="Home" path="/" endPath={true} />
+          <NavItem label="Create Room" path="/create-room" endPath={true} />
+          <NavItem label="Profile" path="/profile" endPath={true} />
+          <NavItem label="Login" path="/login" endPath={true} />
+          <NavItem label="Sign Up" path="/signup" endPath={true} />
+        </nav>
+      </div>
     </header>
   );
 }
