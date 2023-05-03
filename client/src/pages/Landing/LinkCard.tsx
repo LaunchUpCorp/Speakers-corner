9eb943e9d7a3829479e37b21eca2dd908f361f79
Moin Sekander <moinuddin8510@gmail.com>
2023-05-03 16:43:28 -0400

    refactor: add width limits, improve card icon responsiveness via css tweaks

--- client/src/pages/Landing/LinkCard.tsx
+++ client/src/pages/Landing/LinkCard.tsx
@@ -9,13 +9,11 @@ interface Props {
 function LinkCard({ label, path, children }: Props) {
   return (
     <Link
-      className="relative flex h-1/2 min-h-[14.5rem] w-full animate-cardHoverOff flex-col items-center justify-center rounded-xl border-2 border-sky-300 bg-sc-blue text-white hover:animate-cardHover hover:bg-sc-dark-blue md:h-full md:min-h-[27.5rem]"
+      className="flex h-1/2 min-h-[14.5rem] w-full animate-cardHoverOff flex-col items-center justify-center gap-6 rounded-xl border-2 border-sky-300 bg-sc-blue text-white hover:animate-cardHover hover:bg-sc-dark-blue md:h-full md:min-h-[27.5rem] md:gap-10"
       to={path}
     >
-      <div className="mb-10 scale-[5.75]">{children}</div>
-      <h2 className="absolute bottom-6 text-xl uppercase md:bottom-28 md:text-2xl">
-        {label}
-      </h2>
+      {children}
+      <h2 className="text-2xl uppercase md:text-3xl">{label}</h2>
     </Link>
   );
 }
--- client/src/pages/Landing/index.tsx
+++ client/src/pages/Landing/index.tsx
@@ -4,20 +4,22 @@ import LinkCard from "./LinkCard";
 
 function Landing() {
   return (
-    <main>
+    <>
       <Header />
-      <div className="flex min-h-[calc(100vh-6.875)] flex-col items-center gap-4 p-4 md:min-h-[calc(100vh-4.5rem)]">
-        <h1 className="text-3xl font-bold text-gray-700">Home</h1>
-        <div className="flex h-[calc(100vh-12.125rem)] w-full flex-col gap-4 md:h-[calc(100vh-9.75rem)] md:flex-row">
-          <LinkCard label="Create Room" path="/create-room">
-            <AddCircle />
-          </LinkCard>
-          <LinkCard label="View Profile" path="/profile">
-            <AccountCircle />
-          </LinkCard>
+      <main className="flex justify-center">
+        <div className="flex min-h-[calc(100vh-6.875)] w-full max-w-7xl flex-col items-center gap-4 p-4 md:min-h-[calc(100vh-4.5rem)]">
+          <h1 className="text-3xl font-bold text-gray-700">Home</h1>
+          <div className="flex h-[calc(100vh-12.125rem)] w-full flex-col gap-4 text-[7.5rem] md:h-[calc(100vh-9.75rem)] md:flex-row md:text-[10rem]">
+            <LinkCard label="Create Room" path="/create-room">
+              <AddCircle fontSize="inherit" />
+            </LinkCard>
+            <LinkCard label="View Profile" path="/profile">
+              <AccountCircle fontSize="inherit" />
+            </LinkCard>
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
 
