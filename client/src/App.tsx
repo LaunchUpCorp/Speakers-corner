822107c09ef5f455f4ea03d3b5657bd05e6b1a60
Moin Sekander <109125797+m-sekander@users.noreply.github.com>
2023-05-05 18:30:07 -0400

    [SPE-14] create landing UI page (#4)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Landing from "./pages/Landing";
 import RoleAssignmentPage from "./pages/RoleAssignmentPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <main>
-              <Header />
-              <div className="flex min-h-[calc(100vh-4.5rem)] items-center justify-center">
-                <h1 className="rounded-xl border bg-gray-200 px-8 py-4 text-lg font-bold text-gray-700 shadow">
-                  Hello World !
-                </h1>
-              </div>
-            </main>
-          }
-        />
+        <Route path="/" element={<Landing />} />
         <Route
           path="*"
           element={
@@ -32,8 +21,7 @@ function App() {
             </main>
           }
         />
-        <Route path="/role-assignment"
-          element={<RoleAssignmentPage />} />
+        <Route path="/role-assignment" element={<RoleAssignmentPage />} />
       </Routes>
     </BrowserRouter>
   );
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,6 +5,29 @@ module.exports = {
     extend: {
       colors: {
         "sc-blue": "#0077c0",
+        "sc-dark-blue": "#00629e",
+      },
+      keyframes: {
+        cardHover: {
+          "0%": {
+            transform: "scale(1)",
+          },
+          "100%": {
+            transform: "scale(1.0125)",
+          },
+        },
+        cardHoverOff: {
+          "0%": {
+            transform: "scale(1.0125)",
+          },
+          "100%": {
+            transform: "scale(1)",
+          },
+        },
+      },
+      animation: {
+        cardHover: "cardHover 0.375s both",
+        cardHoverOff: "cardHoverOff 0.375s",
       },
     },
   },
