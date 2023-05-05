855ec29183781d8f8ae6ead909650fc83afbc7e9
Kodai Hiraishi <khiraishi0319@gmail.com>
2023-05-05 16:21:50 -0700

    [SPE-9] Chore: create a boilerplate project for backend  (#6)

---
+++ server/src/app.module.ts
@@ -0,0 +1,22 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { HelloModule } from './hello/hello.module';
+
+
+@Module({
+  imports: [
+    HelloModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+        outputAs: 'class'
+      },
+      playground: true
+    })
+  ],
+})
+export class AppModule {}
