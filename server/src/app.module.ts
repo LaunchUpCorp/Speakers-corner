f22a4fb0f0368ada70d290f2279fc484675f392e
khishh <khiraishi0319@gmail.com>
2023-05-05 11:59:30 -0700

    chore: use graphql and remove rest related files

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,22 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { HelloModule } from './hello/hello.module';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    HelloModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+        outputAs: 'class'
+      },
+      playground: true
+    })
+  ],
 })
 export class AppModule {}
