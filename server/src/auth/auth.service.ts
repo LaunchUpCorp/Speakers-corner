3bcdee31de22d8cea47b56b03301d66d4b373d5b
khishh <khiraishi0319@gmail.com>
2023-05-07 14:41:49 -0700

    feat: hash user password and save it in db instead of plaintext

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -2,17 +2,22 @@ import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { UsersService } from "src/users/users.service";
 import { JwtService } from "@nestjs/jwt";
 import { Auth } from "src/graphql";
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(
     private userService: UsersService,
-    private jwtService: JwtService
+    private jwtService: JwtService,
   ) {}
 
-  async signIn(email: string, password: string): Promise<Auth> {
+  private readonly saltOrRounds = 10;
+
+  async signIn(email: string, password: string): Promise<Auth> { 
     const user = await this.userService.findUser(email);
-    if (user?.password !== password) {
+    
+    // Check if the password matched with the hased password saved in db. If not, return Unauthorized response.
+    if(!await bcrypt.compare(password, user?.password)) {
       throw new UnauthorizedException();
     }
 
@@ -30,9 +35,12 @@ export class AuthService {
     try {
       return await this.signIn(email, password);
     } catch (e) {
+      const hashedPassword = await bcrypt.hash(password, this.saltOrRounds);
+      console.log(hashedPassword);
+      
       const newUser = await this.userService.createUser({
         email: email,
-        password: password,
+        password: hashedPassword,
       });
 
       const payload = {
