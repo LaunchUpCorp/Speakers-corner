7069f11c8ee5958494e1302d6296e8fb035f286e
khishh <khiraishi0319@gmail.com>
2023-05-07 14:44:18 -0700

    chore: remove console log statement

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -2,22 +2,22 @@ import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { UsersService } from "src/users/users.service";
 import { JwtService } from "@nestjs/jwt";
 import { Auth } from "src/graphql";
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from "bcrypt";
 
 @Injectable()
 export class AuthService {
   constructor(
     private userService: UsersService,
-    private jwtService: JwtService,
+    private jwtService: JwtService
   ) {}
 
   private readonly saltOrRounds = 10;
 
-  async signIn(email: string, password: string): Promise<Auth> { 
+  async signIn(email: string, password: string): Promise<Auth> {
     const user = await this.userService.findUser(email);
-    
+
     // Check if the password matched with the hased password saved in db. If not, return Unauthorized response.
-    if(!await bcrypt.compare(password, user?.password)) {
+    if (!(await bcrypt.compare(password, user?.password))) {
       throw new UnauthorizedException();
     }
 
@@ -36,8 +36,7 @@ export class AuthService {
       return await this.signIn(email, password);
     } catch (e) {
       const hashedPassword = await bcrypt.hash(password, this.saltOrRounds);
-      console.log(hashedPassword);
-      
+
       const newUser = await this.userService.createUser({
         email: email,
         password: hashedPassword,
